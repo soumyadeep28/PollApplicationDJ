797c2cb1a1379cf91945822b4fb960f5327482fb
ssengup <ssengup@gmail>
2022-01-15 15:43:28 +0530

    views and forms are modified and successfully submitted

--- main/forms.py
+++ main/forms.py
@@ -4,4 +4,4 @@ from main import models
 class AnswerForm(forms.ModelForm):
     class Meta :
         model = models.Answer
-        fields = ['choice' , 'question']
+        fields = ['choice' ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from main import models ,forms
 from django.views.generic import ( ListView ,DetailView , FormView)
-from django.views.generic.detail import BaseDetailView
+from django.views.generic.detail import SingleObjectMixin
 
 from django.http import HttpResponseRedirect
 #these are overrides some function it doesnt handle views 
@@ -15,7 +15,7 @@ class Index(ListView):
  context_object_name = 'questions'
 
 
-class Details(BaseDetailView ,  FormView):
+class Details(SingleObjectMixin ,  FormView):
     model = models.Question
     template_name = 'main/questions.html'
     form_class = forms.AnswerForm  
@@ -23,10 +23,20 @@ class Details(BaseDetailView ,  FormView):
 
     def form_valid(self , form):
         obj = form.save(commit = False)
+        obj.question  = self.get_object()
         obj.user = self.request.user 
         obj.save()
-        
-        return HttpResponseRedirect('/')
+        return HttpResponseRedirect('/main')
+
+    def post(self, request, *args, **kwargs) :
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object)
+
+        return self.render_to_response(context)
         
 
 
